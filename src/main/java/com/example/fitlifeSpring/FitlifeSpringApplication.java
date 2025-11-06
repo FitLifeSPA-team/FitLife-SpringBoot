c696767ee88abe78d6065d9fe81769c1f31046af
SSDD <Alumno@SCAAOB00LC00808.duoc.acad>
2025-11-06 10:47:36 -0300

    Inicio SpringBoot de fitlife, exp 03

---
+++ src/main/java/com/example/fitlifeSpring/FitlifeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitlifeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlifeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlifeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitlifeSpring/FitlifeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitlifeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlifeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
